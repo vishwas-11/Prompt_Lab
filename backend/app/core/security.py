be5856dd690228540a03f123a7ff92b90c13e290
Vishwas Charan <vishwascharan11@gmail.com>
2026-04-17 16:52:57 +0530

    removed depricated time method

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,6 +1,6 @@
 import jwt
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +10,7 @@ ALGORITHM = "HS256"
 
 def create_token(data: dict):
     payload = data.copy()
-    payload["exp"] = datetime.utcnow() + timedelta(hours=24)
+    payload["exp"] = datetime.now(timezone.utc) + timedelta(hours=24)
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 def verify_token(token: str):
